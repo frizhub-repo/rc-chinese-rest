bb4916ec3502e2a57a5e39ed80088608738bfa79
nauman306 <m.nauman@frizhub.com>
2021-04-10 20:28:29 +0500

    Toast added

--- src/index.js
+++ src/index.js
@@ -9,17 +9,19 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'react-multi-carousel/lib/styles.css';
 import 'rsuite/lib/styles/index.less';
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import store from "./store";
 import {Provider} from 'react-redux'
 
 
-ReactDOM.render(
+ReactDOM.render([
     <Provider store={store}>
         <App />
     </Provider>,
-    document.getElementById('root')
-);
+    <ToastContainer />,
+],
+    document.getElementById('root'));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
