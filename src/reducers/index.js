54eae7f84e1568bcd81fa6b04487eb45b097f6e7
nauman306 <m.nauman@frizhub.com>
2021-04-12 11:22:17 +0500

    Product reducer added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import {combineReducers} from "redux";
 import orderReducer from "./orderReducer";
+import productReducer from "./productReducer";
 
 export default combineReducers({
-    orders:orderReducer
+    orders:orderReducer,
+    productReducer: productReducer,
 })
