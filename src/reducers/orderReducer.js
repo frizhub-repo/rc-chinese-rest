28760ae3b313228de3e4c4bc38ebbeee8fe97aaf
nauman306 <m.nauman@frizhub.com>
2021-04-12 12:42:02 +0500

    Order reducer

--- src/reducers/orderReducer.js
+++ src/reducers/orderReducer.js
@@ -1,25 +1,46 @@
+const initialState = {
+  total: 0,
+  items: [],
+  minimum: 0,
+  delivery: 0,
+};
 
-const initialState={
-    total:0,
-    items:[],
-    minimum:0,
-    delivery:0
-}
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case "ADD_ITEM":
+      const index1 = state.items
+        .map(function (e) {
+          return e.product;
+        })
+        .indexOf(action.payload.product);
+      if (index1 !== -1) {
+        const items = state.items;
+        items[index1].quantity =
+          parseInt(items[index1].quantity) + parseInt(action.payload.quantity);
+        return { ...state, items };
+      } else {
+        return {
+          ...state,
+          items: state.items.concat(action.payload),
+        };
+      }
+    //   return {
+    //     ...state,
+    //     items: state.items.concat(action.payload),
+    //   };
 
-export default function (state=initialState,action) {
-    switch (action.type) {
-        case 'ADD_ITEM':
-            return{
-                ...state,
-                items: state.items.concat(action.payload)
-            }
-
-        case 'TOTAL':
-            return{
-                ...state,
-            total:state.total+action.payload
-        }
-        default:return state
-
-    }
+    case "TOTAL":
+      return {
+        ...state,
+        total: state.total + action.payload,
+      };
+    case "REMOVE_ORDER_ITEMS":
+      return {
+        ...state,
+        items: [],
+        total: 0,
+      };
+    default:
+      return state;
+  }
 }
