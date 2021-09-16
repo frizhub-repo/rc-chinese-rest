229ffd3d9f4bf5619ae410986e67dab729932f6a
se-Haroon <haroon.rasheed@frizhub.com>
2021-09-16 15:18:41 +0500

    feature: added logout + secured routes

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -1,3 +1,4 @@
+import Modal from "@material-ui/core/Modal";
 import ActionBox from "./ActionBox/ActionBox";
 import * as React from "react";
 import Account from "./Menus/Account";
@@ -9,11 +10,37 @@ import OrderHistory from "./Menus/OrderHistory";
 
 export default function Settings() {
   const [activeStep, setActiveStep] = React.useState(0);
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+
+  function logout() {
+    window.localStorage.removeItem("token");
+    window.location.reload();
+  }
 
   return (
     <div className={classes.root}>
+      <Modal open={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <div className={`${classes.modalStyle} shadow-lg`}>
+          <h5 className={classes.modalText}>Are you sure?</h5>
+          <div className={classes.btnsContainer}>
+            <button
+              className={`${classes.cancelBtn} shadow-md`}
+              onClick={() => setIsModalOpen(false)}
+            >
+              Cancel
+            </button>
+            <button className={`${classes.yesBtn} shadow-md`} onClick={logout}>
+              Yes
+            </button>
+          </div>
+        </div>
+      </Modal>
       <section>
-        <MenuSelector activeStep={activeStep} setActiveStep={setActiveStep} />
+        <MenuSelector
+          activeStep={activeStep}
+          setActiveStep={setActiveStep}
+          setIsModalOpen={setIsModalOpen}
+        />
       </section>
       <section className={classes.contentContainer}>
         {activeStep === 0 && <Account />}
